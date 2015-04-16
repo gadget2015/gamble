8c27c74b4d4873a917fa032dfefe2425432e61a4
unknown <dcly@Q16349.RSV.SE>
2015-04-16 21:50:28 +0200

    Ny startprogram

---
+++ TAanalys-client/changeToJDK1_7.cmd
@@ -0,0 +1,2 @@
+set JAVA_HOME=C:\minaprogram\Java\jdk1.7.0_75
+set Path=C:\MinaProgram\Java\jdk1.7.0_25\bin;%Path%
--- TAanalys-client/runJavaFX_USAoppning delta - Home.cmd
+++ TAanalys-client/runJavaFX_USAoppning delta - Home.cmd
@@ -1,3 +1,2 @@
-set JAVA_HOME=C:\MinaProgram\Java\jdk1.7.0_25
-set Path=C:\MinaProgram\Java\jdk1.7.0_25\bin;%Path%
+call changeToJDK1_7
 java -cp target\taclient-1.0.jar;lib\jfxrt.jar;lib\slf4j-api-1.6.1.jar;lib\slf4j-jdk14-1.6.1.jar org.robert.taanalys.ria.MainApplication
