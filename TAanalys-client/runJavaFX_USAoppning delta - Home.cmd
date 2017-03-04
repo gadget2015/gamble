63d89c8b65279be71ffb1059408dbf14ce897458
Robert Georén Siwerz <DCLY@rsv.se>
2017-03-04 07:24:25 +0100

    Java 8

--- TAanalys-client/runJavaFX_USAoppning delta - Home.cmd
+++ TAanalys-client/runJavaFX_USAoppning delta - Home.cmd
@@ -1,2 +1,2 @@
-call changeToJDK1_7
+rem call changeToJDK1_7
 java -cp target\taclient-1.0.jar;lib\jfxrt.jar;lib\slf4j-api-1.6.1.jar;lib\slf4j-jdk14-1.6.1.jar org.robert.taanalys.ria.MainApplication
