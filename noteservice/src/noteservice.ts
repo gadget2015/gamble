b71e272e1d32a8c0310e59c560ae0c590c4f2f1b
Robert Georén Siwerz <DCLY@RSV.SE>
2019-07-18 06:09:38 +0200

    Anslutning till databasen funkar nu lokalt.

--- noteservice/src/noteservice.ts
+++ noteservice/src/noteservice.ts
@@ -1,4 +1,5 @@
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
+import * as db from 'mysql';
 
 /**
  * The Note service, that handles CRUD operations.
@@ -9,12 +10,37 @@ export class Noteservice {
     }
 
     getNote(req: Request, res: Response) {
-		const id = parseInt(req.params.id, 10);
-		
-		res.status(200).send({
-			success: 'true',
-			message: 'todos retrieved successfully',
-			todos: {'id': id}
-		});
+        const id = parseInt(req.params.id, 10);
+        const con = this.connectToDb();
+        const sql = 'select * from noterepo.note;';
+
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Result: " + result);
+        });
+
+        res.status(200).send({
+            success: 'true',
+            message: 'todos retrieved successfully',
+            todos: {'id': id}
+        });
+    }
+
+    connectToDb() {
+        var con = db.createConnection({
+            host: "localhost",
+            user: "root",
+            password: ""
+        });
+
+        con.connect(function (err) {
+            if (err) {
+                throw err;
+            } else {
+                console.log("Connected to MySQL database.");
+            }
+        });
+
+        return con;
     }
 }
