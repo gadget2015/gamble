b86b4a67660401b6dff00e4485a1a275e4c98150
Robert Georn Siwerz <robert.georen.siwerz@skatteverket.se>
2020-12-08 05:54:19 +0100

    Förbättrade loggning och att auktorisationsbeslut behövs för adminsidan.

--- spelbolagservice/src/AuthorizationMiddleware.ts
+++ spelbolagservice/src/AuthorizationMiddleware.ts
@@ -13,8 +13,10 @@ class AuthorizationMiddleware {
         const mylog = this.logger;
 
         return async function(req, res, next) {
-            if (req.url.startsWith('/bff/v1/spelare/transaktioner/')) {
+            if (req.url.startsWith('/bff/v1/spelare/transaktioner/') ||
+                req.url === '/bff/v1/administration/') {
                 // Användaren mäste vara authentiserad och även administratör
+                mylog.debug('Detta anrop kräver att användaren är aukotriserad med administratör behörighet.');
                 const userid = req['userid'];
                 const service = new Spelbolagservice(mylog);
                 const spelareResult = await service.getSpelare(userid);
@@ -22,6 +24,7 @@ class AuthorizationMiddleware {
 
                 if(spelare.administratorforspelbolag_id !== null) {
                     // Den inloggad är administratör för ett spelbolag = administratör = OK gå vidare.
+                    mylog.debug('Användaren (' + userid + ') är administratör för ett spelbolag.')
                     next();
                 } else {
                     // Inloggad är inte administratör.
