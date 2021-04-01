378c1f741f086c6b744ab83d9e507a79fbe62ee5
Robert Georn Siwerz <robert.georen.siwerz@skatteverket.se>
2021-04-01 09:16:10 +0200

    Removed unused code.

--- swingGUI/src/test/java/org/robert/tips/stryktips/domain/model/PlayedPercentageTest.java
+++ swingGUI/src/test/java/org/robert/tips/stryktips/domain/model/PlayedPercentageTest.java
@@ -8,145 +8,11 @@ import org.robert.tips.exceptions.GeneralApplicationException;
 import org.robert.tips.exceptions.NoReducedRowsException;
 import org.robert.tips.exceptions.ReducingParametersNotSetException;
 import org.robert.tips.stryktips.exceptions.GameNotSetException;
-import org.robert.tips.stryktips.types.StryktipsGame;
 
 import static org.robert.tips.stryktips.types.StryktipsConstants.*;
-import java.util.ArrayList;
-import java.util.Iterator;
+
 
 public class PlayedPercentageTest {
-
-    //@Test
-    public void shouldBeOneRowLeftAfterReduced() throws GameNotSetException, NoReducedRowsException, GeneralApplicationException, ReducingParametersNotSetException {
-        // Given
-        StryktipsSystem stryktipsSystem = new StryktipsSystem();
-        PlayedPercentage played = initTestdata(stryktipsSystem);
-        played.revenue = 2800000;
-        played.minimumNumberOfPeopleWithFullPot = 60000;
-        played.maxiumumNumberOfPeopleWithFullPot = 70000;
-
-        // When
-        played.reduce();
-        int rows = stryktipsSystem.getReducedSystem().size();
-
-        // Then
-        Assert.assertEquals("Should be one row after reduced.", 1, rows);
-    }
-
-    //@Test
-    public void shouldBeThreeRowsLeftAfterReduced() throws GameNotSetException, NoReducedRowsException, GeneralApplicationException, ReducingParametersNotSetException {
-        // Given
-        StryktipsSystem stryktipsSystem = new StryktipsSystem();
-        PlayedPercentage played = initTestdata(stryktipsSystem);
-        played.revenue = 2800000;
-        played.minimumNumberOfPeopleWithFullPot = 85000;
-        played.maxiumumNumberOfPeopleWithFullPot = 100000;
-
-        // When
-        played.reduce();
-        int rows = stryktipsSystem.getReducedSystem().size();
-
-        // Then
-        Assert.assertEquals("Should be two row after reduced.", 3, rows);
-    }
-
-    //@Test
-    public void shouldUseMaxValueWhenReducing() throws GameNotSetException, NoReducedRowsException, GeneralApplicationException, ReducingParametersNotSetException {
-        // Given
-        StryktipsSystem stryktipsSystem = new StryktipsSystem();
-        PlayedPercentage played = initTestdata(stryktipsSystem);
-        played.revenue = 2800000;
-        played.minimumNumberOfPeopleWithFullPot = 350000;
-        played.maxiumumNumberOfPeopleWithFullPot = 360000;
-
-        // When
-        played.reduce();
-        int rows = stryktipsSystem.getReducedSystem().size();
-
-        // Then
-        Assert.assertEquals("Should be one row after reduced.", 1, rows);
-    }
-
-    //@Test
-    public void shouldUseToLowRangeWhenReducing() throws GameNotSetException, NoReducedRowsException, GeneralApplicationException, ReducingParametersNotSetException {
-        // Given
-        StryktipsSystem stryktipsSystem = new StryktipsSystem();
-        PlayedPercentage played = initTestdata(stryktipsSystem);
-        played.revenue = 2800000;
-        played.minimumNumberOfPeopleWithFullPot = 20000;
-        played.maxiumumNumberOfPeopleWithFullPot = 40000;
-
-        // When
-        played.reduce();
-        int rows = stryktipsSystem.getReducedSystem().size();
-
-        // Then
-        Assert.assertEquals("Should be NO row after reduced.", 0, rows);
-    }
-
-    //@Test
-    public void shouldUseToHighRangeWhenReducing() throws GameNotSetException, NoReducedRowsException, GeneralApplicationException, ReducingParametersNotSetException {
-        // Given
-        StryktipsSystem stryktipsSystem = new StryktipsSystem();
-        PlayedPercentage played = initTestdata(stryktipsSystem);
-        played.revenue = 2800000;
-        played.minimumNumberOfPeopleWithFullPot = 400000;
-        played.maxiumumNumberOfPeopleWithFullPot = 500000;
-
-        // When
-        played.reduce();
-        int rows = stryktipsSystem.getReducedSystem().size();
-
-        // Then
-        Assert.assertEquals("Should be NO row after reduced.", 0, rows);
-    }
-
-    //@Test
-    public void shouldMissNumberOfPeopleRange() throws GameNotSetException, NoReducedRowsException, GeneralApplicationException, ReducingParametersNotSetException {
-        // Given
-        StryktipsSystem stryktipsSystem = new StryktipsSystem();
-        PlayedPercentage played = initTestdata(stryktipsSystem);
-        played.revenue = 2800000;
-        played.minimumNumberOfPeopleWithFullPot = -12;
-        played.maxiumumNumberOfPeopleWithFullPot = 0;
-
-        // When
-        try {
-            played.reduce();
-            // Then
-            Assert.fail("Should miss a parameter.");
-        }
-        catch(ReducingParametersNotSetException e){
-
-        }
-    }
-
-    private PlayedPercentage initTestdata(StryktipsSystem stryktipsSystem) throws GameNotSetException {
-        setMathematicalsystemData(2,0, stryktipsSystem);
-        stryktipsSystem.getMathimatical().reduce();
-        //printReducedRows(stryktipsSystem.getReducedSystem());
-
-        PlayedPercentage played = stryktipsSystem.getPlayed();
-        played.setPercentage(0, 0.2f);
-        played.setPercentage(1, 0.3f);
-        played.setPercentage(2, 0.5f);
-        played.setPercentage(3, 0.2f);
-        played.setPercentage(4, 0.3f);
-        played.setPercentage(5, 0.5f);
-        for (int i= 6; i < 39; i++) {
-            played.setPercentage(i, 1); // Fill rest with ones for easier assertions.
-        }
-
-        // In resulting system that consists of 4 rows, these are the probability (P).
-        // row #1 P = 0.2*0.2 = 0.04
-        // row #2 P = 0.2*0.3 = 0.06
-        // row #3 P = 0.3*0.2 = 0.06
-        // row #4 P = 0.3*0.3 = 0.09
-
-        played.koefficientMin = 0.5f;
-        played.koefficientMax = 1.5f;
-        return played;
-    }
 
     @Test
     public void shouldBe276Rows() throws GameNotSetException, NoReducedRowsException, GeneralApplicationException, ReducingParametersNotSetException {
@@ -195,6 +61,7 @@ public class PlayedPercentageTest {
             e.printStackTrace();
         }
     }
+
     private PlayedPercentage initM_3_10_27648_SystemTestdata(StryktipsSystem stryktipsSystem) throws GameNotSetException {
         setMathematicalsystemData(10,3, stryktipsSystem);
         stryktipsSystem.getMathimatical().reduce();
@@ -242,17 +109,6 @@ public class PlayedPercentageTest {
         played.setPercentage(38, 0.84f);
 
         return played;
-    }
-
-    private void printReducedRows(ArrayList rows) {
-        Iterator iterator = rows.iterator();
-        int i = 0;
-        while (iterator.hasNext()) {
-            StryktipsGame game = (StryktipsGame) iterator.next();
-            char[] singleRow = game.getSingleRow();
-
-            System.out.println("Row #" + i++ + ": " + game.toString()) ;
-        }
     }
 
     private void setMathematicalsystemData(int numberOfHalfHedging, int numberOfHoleHedging, StryktipsSystem stryktipsSystem) {
